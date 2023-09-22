bbfcde25f58abed7a4885767ab5068b15d383b42
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-09-23 00:34:26 +0300

    gsm get http part 1

--- gsm/gsm.c
+++ gsm/gsm.c
@@ -1,7 +1,7 @@
 #include "gsm.h"
+#include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include "hardware/gpio.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -28,25 +28,17 @@ void setup_gsm(int uart,int baudrate, int tx, int rx, int dtr)
 
     uart_puts(uart > 0 ? uart1 : uart0,"AT");
 
-    char status[2];
+    int status;
+    uart_read_ok(&status);
 
-    for (int i = 0; i < 2; i++)
+    if (status)
     {
-        if (uart_is_readable(uart > 0 ? uart1 : uart0))
-        {
-            status[i] = uart_getc(uart > 0 ? uart1 : uart0);
-        }
-    }
-
-    if(strcmp(status,"OK") == 0)
-    { 
-        printf("Status: OK");
+        printf("STATUS OK");
     }
     else
     {
-        printf("Status: SIM WORKS BUT NOT OK CANT COMMUNICATE");
+        printf("STATUS NOT OK");
     }
-
 
     txPin = tx;
     rxPin = rx;
@@ -70,3 +62,66 @@ void enable_sleep()
     }
 }
 
+void get_request(char ip[], char* result[])
+{
+//TCP CONNECTION
+    char *base = 'AT+CIPSTART="TCP","",80', *tcpCommand;
+    int location = 19; // starts from 1 not 0
+    
+    insert_string(&tcpCommand, location, &base);
+
+    uart_puts(uartInterface > 0 ? uart1 : uart0, tcpCommand);
+
+    int status;
+    uart_read_ok(&status);
+
+    if (status)
+    {
+        printf("CTP CONNECTION SUCCESFUL");
+    }
+    else
+    {
+        printf("ERROR CANT CONNECT TCP");
+    }
+
+//endregion
+}
+
+void insert_string(char* destination, int pos, char* insert)
+{
+    char *strC;
+
+    strC = (char*)malloc(strlen(destination) + strlen(insert) + 1);
+    strncpy(strC, destination, pos);
+    strC[pos] = '\0';
+    strcat(strC, insert);
+    strcat(strC, destination + pos);
+    strcpy(destination, strC);
+    free(strC);
+}
+
+
+void uart_read_ok(int* isOk)
+{
+    char* readed = malloc(sizeof(char)*2);
+    int index = 0;
+    while (index < 2)
+    {
+        if (uart_is_readable(uartInterface > 0 ? uart1 : uart0))
+        {
+            readed[index] = uart_getc(uartInterface > 0 ? uart1 : uart0);
+            index++;
+        }
+    } 
+
+    if (strcmp(readed, "OK") == 0)
+    {
+        *isOk = 1;
+    }
+    else
+    {
+        *isOk = 0;
+    }
+
+    free(readed);
+}
--- gsm/gsm.h
+++ gsm/gsm.h
@@ -1,6 +1,7 @@
 #ifndef GSM_H
 #define GSM_H
 
+
 static int txPin, rxPin, dtrPin;
 
 void setup_gsm(int uart,int baudrate,int tx, int rx, int dtr); //SET DTR -1 IF YOU DONT WANT TO USE SLEEP FEATURE ALSO INIT STDIO BEFORE CALLING FUNCTION
@@ -8,8 +9,12 @@ void setup_gsm(int uart,int baudrate,int tx, int rx, int dtr); //SET DTR -1 IF Y
 void disable_sleep();
 void enable_sleep();
 
-void get_request(char url[],char *result[]);
+void get_request(char ip[],char* result[]);
 
 void send_sms(char num[], char message[]);
+
+void insert_string(char* destination, int pos, char* insert);
+
+void uart_read_ok(int* isOk);
 
 #endif // !GSM_H
